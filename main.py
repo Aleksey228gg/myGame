55f4da4976051ae9183f22438b1e1711b87875b8
MrPutis <fantazer228gg@gmail.com>
2025-01-18 21:31:26 +0300

    add bug

---
+++ main.py
@@ -0,0 +1,35 @@
+from symtable import Class
+
+import pygame
+
+FPS = 15
+TILE_SIZE = 40
+
+class Map_editor:
+    def __init__(self, map_name, free_tile, finish_tile):
+        self.map = []
+        with open("maps/" + map_name) as map_file:
+            for line in map_file:
+                self.map.append(list(map(int, line.split())))
+        self.height = len(self.map)
+        self.width = len(self.map[0])
+        self.tile_size = TILE_SIZE
+        self.free_tiles = free_tile
+        self.finish_tile = finish_tile
+
+
+if __name__ == '__main__':
+    pygame.init()
+    pygame.display.set_caption('')
+    size = width, height = 1200, 800
+    screen = pygame.display.set_mode(size)
+
+    running = True
+    clock = pygame.time.Clock()
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+    pygame.quit()
